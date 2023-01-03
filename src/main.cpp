5b26299d114460edacbaa19853cd12d0c1ed1563
jamesbbun <jamesbbun@gmail.com>
2023-01-03 09:58:37 +0900

    첫번째 커밋. 메인 함수 작성

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char** argv)
+{
+    SPDLOG_INFO("Start program");
+
+    SPDLOG_INFO("Initialize glfw");
+    if(!glfwInit()) {
+        const char* desc = nullptr;
+        glfwGetError(&desc);
+        SPDLOG_ERROR("failed to initialize glfw: {}", desc);
+        return -1;
+    }
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if(!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    SPDLOG_INFO("Terminate");
+    return 0;
+}
