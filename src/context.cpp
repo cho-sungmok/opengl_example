095c2d8fed7f2a32faa7fc548b4d06dd3d6adc21
jamesbbun <jamesbbun@gmail.com>
2023-01-10 19:31:12 +0900

    light ambient spec

--- src/context.cpp
+++ src/context.cpp
@@ -80,36 +80,36 @@ void Context::MouseButton(int button, int action, double x, double y)
 
 bool Context::init()
 {
-	float vertices[] = {
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+	float vertices[] = { // pos.xyz, normal.xyz, texcoord.uv
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f,
 
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f,
 
-		-0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f,
 
-		0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f,
 
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f,
 
-		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f,
 	};
 
 	uint32_t indices[] = {
@@ -122,16 +122,17 @@ bool Context::init()
 	};
 	m_vertexLayout = VertexLayout::Create();
 	m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER,
-		GL_STATIC_DRAW, vertices, sizeof(float) * 120);// 4 x 6 x 5
+		GL_STATIC_DRAW, vertices, sizeof(float) * 8 * 6 * 4);
 
-	m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
-	m_vertexLayout->SetAttrib(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, sizeof(float) * 3);
+	m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+	m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 3);
+	m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 6);
 
 	m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER,
-		GL_STATIC_DRAW, indices, sizeof(uint32_t) * 36);// 6 x 6
+		GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6 * 6);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/lighting.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/lighting.fs", GL_FRAGMENT_SHADER);
     if(!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vert shader id: {}", vertShader->Get());
@@ -190,10 +191,19 @@ void Context::Render()
 			m_cameraYaw = 0.0f;
 			m_cameraPitch = 0.0f;
 			m_cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+		}		
+		if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::DragFloat3("light pos", glm::value_ptr(m_lightPos), 0.01f);
+			ImGui::ColorEdit3("light color", glm::value_ptr(m_lightColor));
+			ImGui::ColorEdit3("object color", glm::value_ptr(m_objectColor));
+			ImGui::SliderFloat("ambient strength", &m_ambientStrength, 0.0f, 1.0f);
+			ImGui::SliderFloat("specular strength", &m_specularStrength, 0.0f, 1.0f);
+			ImGui::DragFloat("specular shiness", &m_specularShiness, 1.0f, 1.0f, 256.0f);
 		}
+		ImGui::Checkbox("animation", &m_animation);
 	}
 	ImGui::End();
-	
+
 	std::vector<glm::vec3> cubePositions = {
 		glm::vec3( 0.0f, 0.0f, 0.0f),
 		glm::vec3( 2.0f, 5.0f, -15.0f),
@@ -222,15 +232,37 @@ void Context::Render()
 	auto view = glm::lookAt(m_cameraPos,
 		m_cameraPos + m_cameraFront, m_cameraUp);
 
+	auto lightModelTransform =
+		glm::translate(glm::mat4(1.0), m_lightPos) *
+		glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
+	m_program->Use();
+	m_program->SetUniform("viewPos", m_cameraPos);
+	m_program->SetUniform("lightPos", m_lightPos);
+	m_program->SetUniform("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+	m_program->SetUniform("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
+	m_program->SetUniform("ambientStrength", 1.0f);
+	m_program->SetUniform("specularStrength", m_specularStrength);
+	m_program->SetUniform("specularShiness", m_specularShiness);
+	m_program->SetUniform("transform", projection * view * lightModelTransform);
+	m_program->SetUniform("modelTransform", lightModelTransform);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	m_program->Use();
+	m_program->SetUniform("lightPos", m_lightPos);
+	m_program->SetUniform("lightColor", m_lightColor);
+	m_program->SetUniform("objectColor", m_objectColor);
+	m_program->SetUniform("ambientStrength", m_ambientStrength);
+
 	for(size_t i=0; i<cubePositions.size(); i++)
 	{
 		auto& pos = cubePositions[i];
 		auto model = glm::translate(glm::mat4(1.0f), pos);
 		model = glm::rotate(model,
-			glm::radians((float)glfwGetTime()*120 + 20 * i),
+			glm::radians((m_animation ? (float)glfwGetTime() : 0.0f) * 120 + 20 * i),
 			glm::vec3(1.0f, 0.5f, 0.0f));
 		auto transform = projection * view * model;
 		m_program->SetUniform("transform", transform);
+		m_program->SetUniform("modelTransform", model);
 		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);// 6 x 6
 	}
 }
--- src/context.h
+++ src/context.h
@@ -28,11 +28,19 @@ private:
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
 
+	bool m_animation { true };
+
 	glm::vec4 m_clearColor { glm::vec4(0.1f, 0.2f, 0.3f, 0.0f) };
+
+	glm::vec3 m_lightPos { glm::vec3(3.0f, 3.0f, 3.0f) };
+	glm::vec3 m_lightColor { glm::vec3(1.0f, 1.0f, 1.0f) };
+	glm::vec3 m_objectColor { glm::vec3(1.0f, 0.5f, 0.0f) };
+	float m_ambientStrength { 0.1f };
+	float m_specularStrength { 0.5f };
+	float m_specularShiness { 32.0f };
 
 	bool m_cameraControl { false };
 	glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
-
 	float m_cameraPitch { 0.0f };
 	float m_cameraYaw { 0.0f };
 	glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
